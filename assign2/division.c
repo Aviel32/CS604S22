045bc82f888a6e9b4f801606115dc7e18e5979a8
Jonathan <js11283n@pace.edu>
2022-02-14 13:27:05 -0500

    Added file into file to push into git hub with all code completed

---
+++ assign2/division.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+
+int main() {
+    
+	float _res;
+    int _i = 3;
+    int _p = 2;
+
+    _res =  (float)_i / (float)_p;
+    //This prints out 1.000000 because the / is an integer division
+    //TODO: please modify the expression above (in line 9) so the division yields a real number (1.500000 in this case) WITHOUT changing the declared type of _i or _p.
+    //hint: try to make either divisor or dividend a float type
+    
+    printf("%f\n", _res);
+    
+    /*Or just cast one of the variables and that will also give you the result you are looking for */
+    return 0;
+    
+}
